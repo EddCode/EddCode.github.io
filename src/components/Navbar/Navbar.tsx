0d88205bb51a42892b248ab8bd2596a2f1475a86
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2024-12-20 17:42:57 -0600

    Change menu to be a column when open and add a gap between items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -28,66 +28,70 @@ export default function Navbar(prop: { url?: MenuItem }) {
     return (
 
         <nav className={style.nav}>
-            <div className={style['nav-content']}>
+            <div className={`${isOpenMenu ? `${style['nav-content']} ${style.open}` : style['nav-content']}`}>
                 <h2 className={style.title}>EDGAR FIGUEROA</h2>
                 <div className={ isOpenMenu ? `${style.close} ${style.burger}` : style.burger} onClick={toogleMenu}>
                     <div className={style['btn-line']}></div>
                     <div className={style['btn-line']}></div>
                     <div className={style['btn-line']}></div>
                 </div>
-                <ul className={style.items}>
-                    <li className={style['nav-item']}>
-                        <a 
-                            href="/#home"
-                            data-item={MenuItem.HOME}
-                            className={item == MenuItem.HOME ? style['is-active'] : ''}
-                            onClick={handleClick}
-                        >
-                            Home
-                        </a>
-                    </li>
-                    <li className={style['nav-item']}>
-                        <a 
-                            href="/#about"
-                            data-item={MenuItem.ABOUT}
-                            className={item == MenuItem.ABOUT ? style['is-active'] : ''}
-                            onClick={handleClick}
-                        >
-                            About me
-                        </a>
-                    </li>
-                    <li className={style['nav-item']}>
-                        <a 
-                            href="/#skills" 
-                            data-item={MenuItem.SKILLS}
-                            className={item == MenuItem.SKILLS ? style['is-active'] : ''}
-                            onClick={handleClick}
-                        >
-                            Skills
-                        </a>
-                    </li>
-                    <li className={style['nav-item']}>
-                        <a 
-                            href="/#experience" 
-                            data-item={MenuItem.EXPERIENCE}
-                            className={item == MenuItem.EXPERIENCE ? style['is-active'] : ''}
-                            onClick={handleClick}
-                        >
-                            Work Experience
-                        </a>
-                    </li>
-                    <li className={style['nav-item']}>
-                        <a 
-                            data-item={MenuItem.BLOG}
-                            href='/blog'
-                            className={item == MenuItem.BLOG ? style['is-active'] : ''}
-                            onClick={handleClick}
-                        >
-                            Blog
-                        </a>
-                    </li>
-                </ul>
+                <ListMenu item={item} handleClick={handleClick} isOpen={isOpenMenu}/>
             </div> 
         </nav>
     )
 }
+
+const ListMenu = ({ item, handleClick, isOpen}: { isOpen: boolean, item: MenuItem, handleClick: (evt: Event) => void}) => (
+    <ul className={isOpen ? style['items-open'] : style.items}>
+        <li className={style['nav-item']}>
+            <a 
+                href="/#home"
+                data-item={MenuItem.HOME}
+                className={item == MenuItem.HOME ? style['is-active'] : ''}
+                onClick={handleClick}
+            >
+                Home
+            </a>
+        </li>
+        <li className={style['nav-item']}>
+            <a 
+                href="/#about"
+                data-item={MenuItem.ABOUT}
+                className={item == MenuItem.ABOUT ? style['is-active'] : ''}
+                onClick={handleClick}
+            >
+                About me
+            </a>
+        </li>
+        <li className={style['nav-item']}>
+            <a 
+                href="/#skills" 
+                data-item={MenuItem.SKILLS}
+                className={item == MenuItem.SKILLS ? style['is-active'] : ''}
+                onClick={handleClick}
+            >
+                Skills
+            </a>
+        </li>
+        <li className={style['nav-item']}>
+            <a 
+                href="/#experience" 
+                data-item={MenuItem.EXPERIENCE}
+                className={item == MenuItem.EXPERIENCE ? style['is-active'] : ''}
+                onClick={handleClick}
+            >
+                Work Experience
+            </a>
+        </li>
+        <li className={style['nav-item']}>
+            <a 
+                data-item={MenuItem.BLOG}
+                href='/blog'
+                className={item == MenuItem.BLOG ? style['is-active'] : ''}
+                onClick={handleClick}
+            >
+                Blog
+            </a>
+        </li>
+    </ul>
+)
