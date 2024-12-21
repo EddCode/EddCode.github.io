d2cb2de48a8c53ef974182f7cb050bcfb825059f
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2024-12-21 01:24:23 -0600

    try shared image

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Edgar Figueroa';
 export const SITE_DESCRIPTION = 'Software engineer with extensive experience since 2018, specializing in backend development, clean architectures, and scalable solutions. Proficient in Node.js, TypeScript, and modern software engineering practices. Explore my portfolio, blog, and insights on cutting-edge development techniques.';
 export const BLOG_DESCRIPTION = '“A blog dedicated to sharing insights, best practices, and experiences in software development. Topics include backend engineering, clean architectures, DDD, microservices, and tips for mastering Node.js and TypeScript. Perfect for developers looking to enhance their skills and stay updated on modern software trends.'
